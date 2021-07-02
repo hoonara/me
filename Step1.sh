845defc81aaa7e609d5adea4e48f54588b8b48ad
Hoon <39038198+hoonara@users.noreply.github.com>
2021-07-02 14:42:01 +0200

    Update Step1.sh

--- Step1.sh
+++ Step1.sh
@@ -31,48 +31,9 @@ run () {
     
     echo ""
     echo "Reboot the machine"
-    Sleep 5s
     sudo reboot now
     
-    
-    
-    #export GOPATH=$HOME/go
-    #export GOBIN=$GOPATH/bin
-    #export PATH=$PATH:$GOBIN:/usr/local/go/bin
-
-    #echo "Installing cosmos-sdk"
-
-    #mkdir -p $GOPATH/src/github.com/cosmos
-    #cd $GOPATH/src/github.com/cosmos
-    #git clone https://github.com/cosmos/cosmos-sdk
-    #cd cosmos-sdk && git checkout $COSMOS_VERSION
-    #make tools install
-
-    #gaiad version --long
-    #gaiacli version --long
-
-    #gaiacli config node mcv-sentry-1.mycosmosvalidator.com:26657
-    #gaiacli config trust-node true
-    #gaiacli config chain-id cosmoshub-2
-
-
-    #echo "Gaiacli and Gaiad successfully installed"
-    #echo ""
-    #echo ""
-    #echo "NOTE PLEASE RUN"
-    #echo ""
-    #echo "source ~/.bashrc"
-    #echo ""
-    #echo "to refresh your paths"
-    #echo ""
-    #echo ""
-    #echo "For more information and help on how to use gaiacli to send transactions and delegate, please visit https://mycosmosvalidator.com"
-    #echo ""
-    #echo "Try running gaiacli status to see the current status of the cosmos network"
-    #echo ""
-
-
-
+ 
 }
 
 run
